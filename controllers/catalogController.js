93d76a7ffdcfd21624f7698fbdd3ee2f0d8799a9
GeorgiGeorgiev-91 <gogoveca1@gmail.com>
2022-09-22 08:47:54 +0300

    Implemented search functionality

--- controllers/catalogController.js
+++ controllers/catalogController.js
@@ -3,10 +3,20 @@ const { getAll, getById } = require('../services/accomodationService');
 const router = require('express').Router();
 
 router.get('/', (req, res) => {
-    const rooms = getAll();
+    console.log(req.query);
+    const search = req.query.search || '';
+    const city = req.query.city || '';
+    const fromPrice = Number(req.query.fromPrice) || 1;
+    const toPrice = Number(req.query.toPrice) || 1000;
+
+    const rooms = getAll(search, city, fromPrice, toPrice);
     res.render('catalog', {
         title: 'All Accomodation',
-        rooms
+        rooms,
+        search,
+        city,
+        fromPrice,
+        toPrice
     })
 })
 
