4188ab71e0339a4064a03e7da15f6f8e1e8ddfdd
GeorgiGeorgiev-91 <gogoveca1@gmail.com>
2022-10-05 19:38:28 +0300

    Add Facility Editor

--- services/facilityService.js
+++ services/facilityService.js
@@ -1,4 +1,5 @@
 const Facility = require('../models/Facility');
+const Room = require('../models/Room');
 
 async function getAllFacilities() {
     return Facility.find({}).lean();
@@ -12,7 +13,33 @@ async function createFacility(label, iconUrl) {
 }
 
 async function addFacilities(roomId, facilityIds) {
+    const room = await Room.findById(roomId).populate('facilities');
+    const facilities = await Facility.find({ _id: { $in: facilityIds } });
 
+    // remove room ref from removed facilities
+    const toRemove = room.facilities.filter(f => facilities.every(x => x._id.toString() != f._id.toString()));
+    // console.log('to remove', toRemove.map(x => x.label));
+    toRemove.forEach(f => {
+        // remove room from facility
+        f.rooms.splice(f.rooms.findIndex(rId => rId.toString() == roomId), 1);
+        // remove facility from room
+        room.facilities.splice(room.facilities.findIndex(x => x._id.toString() == f._id.toString()), 1);
+    })
+
+    // determine new facilities
+    const newlyAdded = facilities.filter(f => room.facilities.every(x => x._id.toString() != f._id.toString()));
+    // console.log('add', newlyAdded.map(x => x.label));
+
+    // add room ref to newly added facilities
+
+    newlyAdded.forEach(f => {
+        room.facilities.push(f);
+        f.rooms.push(room);
+    });
+
+    await room.save();
+    await Promise.all(toRemove.map(f => f.save()));
+    await Promise.all(newlyAdded.map(f => f.save()));
 }
 
 module.exports = {
